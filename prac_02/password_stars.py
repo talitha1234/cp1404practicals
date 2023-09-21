ca3dc3de71760a9f0cfaa88c5da1c457b98bf8f3
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-09-21 14:41:51 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,5 +1,24 @@
-password = input("Password: ")
-while len(password) < 5:
-    print("Invalid password. Must be greater than 5 characters.")
+"""Get password and print asterisks"""
+
+
+def main():
+    """Get password and print asterisks"""
+    password = get_password()
+    prints_asterisks(password)
+
+
+def prints_asterisks(password):
+    """Print asterisks"""
+    print(len(password) * '*')
+
+
+def get_password():
+    """Get password from user"""
     password = input("Password: ")
-print(len(password) * '*')
+    while len(password) < 5:
+        print("Invalid password. Must be greater than 4 characters.")
+        password = input("Password: ")
+    return password
+
+
+main()
