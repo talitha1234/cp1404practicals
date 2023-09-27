94deba6c55a61c193f8dd798d10c79b558f90bb6
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-09-27 13:35:12 +1000

    Add randoms program to demonstrate random module functions ranges

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+# print(random.randint(5, 20))  # line 1
+# print(random.randrange(3, 10, 2))  # line 2
+# print(random.uniform(2.5, 5.5))  # line 3
+
+# 20
+# Could have seen: 5 is the smallest, 20 is the largest
+# 5
+# Could have seen: smallest is 3, largest is 9, line 2 could not have produced a 4 because of the 2 step counter
+# 4.6100487596447
+# smallest number: 2.5 , largest number: 5.5
+import random
+
+print(random.randint(1, 100 + 1))
