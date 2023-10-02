82f00f8252c5ee9f3e3b39fe4ed7f1a51c155aca
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-02 18:46:12 +1000

    Complete list comprehensions to do

--- prac_04/list_comrpehensions.py
+++ prac_04/list_comrpehensions.py
@@ -1,5 +1,4 @@
 """
-CP1404/CP5632 Practical
 List comprehensions
 """
 
@@ -29,16 +28,24 @@ print(a_names)
 # 'Ada Alan Angel Bob Jimi'
 print(" ".join(sorted(names)))
 
-# TODO: list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+# list comprehension to create a list of all the full_names in lowercase format
+lowercase_full_names = [name.lower() for name in full_names]
+print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: list comprehension to create a list of integers from the above list of strings
-# numbers =
+# list comprehension to create a list of integers from the above list of strings
+numbers = [int(number) for number in almost_numbers]
+print(numbers)
 
-# TODO: list comprehension to create a list of only the numbers that are
+# list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created
+numbers_greater_than_9 = [number for number in numbers if number > 9]
+print(numbers_greater_than_9)
 
-# TODO: (more advanced) use a list comprehension and the join string method
+# (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+
+# get 2nd name (split by space) FOR name in full name, only  IF full name longer than 11
+last_name_parts = [(name.split())[1] for name in full_names if len(name) > 11]
+print(last_name_parts)
