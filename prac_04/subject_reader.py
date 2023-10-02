55cc5d1ca8660f91fe0fbff02dc0cf08513486ae
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-02 17:27:45 +1000

    Return data as list to main in subject reader program, and add a new function to show subject detail

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,19 +1,28 @@
 """
-CP1404/CP5632 Practical
 Data file -> lists program
+Displays data from subjects and demonstrates process with print statements and returns to main with list
 """
 
 FILENAME = "subject_data.txt"
 
 
 def main():
+    """Displays data from subjects and demonstrates process with print statements and returns to main with list"""
     data = get_data()
     print(data)
+    display_subject_details(data)
+
+
+def display_subject_details(data):
+    """Displays subject details from data"""
+    for point in data:
+        print(f'{point[0]} is taught by {point[1]} and has {point[2]} students')
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +32,9 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
 
 
 main()
