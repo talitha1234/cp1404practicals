05e272621c4bd838d4426ffc73edf7aaab43668f
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-02 16:43:04 +1000

    Add subject data and subject reader program, reformat string in total income program

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,11 +7,9 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     number_of_months = int(input("How many months? "))
-
     for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
-
     print_report(incomes)
 
 
@@ -23,7 +21,7 @@ def print_report(incomes):
     for income in incomes:
         month += 1
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
