72eece04e98a4d7c9babe18a1116aa814019ceec
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-29 20:28:02 +1000

    Complete guitar class program in prac 6

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,24 +1,32 @@
 """
 Guitar.
 Estimate: 20 mins, 6:27pm start
-Actual: 12 mins, 6:39 finish
+Actual: 15 mins, 6:39 finish, + 3 mins
 """
 import datetime
 
+VINTAGE_AGE = 50
+
 
 class Guitar:
+    """"Represent a Guitar object."""
+
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a car object"""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string representation of data in car."""
         return f'{self.name} ({self.year}) : ${self.cost}'
 
     def get_age(self):
+        """Return the age of a guitar."""
         return datetime.date.today().year - self.year
 
     def is_vintage(self):
-        if self.get_age() > 50:
+        """Return True if guitar is older than 50 years"""
+        if self.get_age() > VINTAGE_AGE:
             return True
         return False
