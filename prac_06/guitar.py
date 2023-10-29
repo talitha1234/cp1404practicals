9eb6637f54606332f69f0ca8cebc8294bb7fe54c
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-29 18:41:36 +1000

    Complete guitar class program in prac 6

---
+++ prac_06/guitar.py
@@ -0,0 +1,27 @@
+"""
+Guitar.
+Estimate: 20 mins, 6:27pm start
+Actual: 12 mins, 6:39 finish
+"""
+import datetime
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f'{self.name} ({self.year}) : ${self.cost}'
+
+    def get_age(self):
+        return datetime.date.today().year - self.year
+
+    def is_vintage(self):
+        if guitar.get_age() > 50:
+            return True
+        return False
+
+
+guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
