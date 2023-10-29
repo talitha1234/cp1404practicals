346b5e34161d84f5d5f658fcefb08f0be768397c
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-29 16:07:28 +1000

    Complete car and used cars modifications in prac 6

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,17 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
-    print(f"Car has fuel: {my_car.fuel}")
+    print(f"{my_car.name} has fuel: {my_car.fuel}")
     print(my_car)
+    limo = Car("Limo", 100)
+    limo.fuel += 20
+    print(f'{limo.name} has fuel: {limo.fuel}')
+    limo.drive(115)
+    print(limo)
+    
+
 
 
 main()
