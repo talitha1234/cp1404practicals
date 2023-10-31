073c4599a876fef0f1530fe87bb2574d8e419f26
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-10-31 16:40:14 +1000

    Add is vintage test to guitar tests in prac 6

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -7,5 +7,7 @@ from prac_06.guitar import Guitar
 
 guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
 print(f'{guitar.name} get_age() - Expected 101. Got {guitar.get_age()}')
-another_guitar = Guitar("Another Guitar", 2020, 1500.35)
-print(f'{another_guitar.name} get_age() - Expected 3. Got {another_guitar.get_age()}')
+another_guitar = Guitar("Another Guitar", 1973, 1500.35)
+print(f'{another_guitar.name} get_age() - Expected 50. Got {another_guitar.get_age()}')
+print(f'{guitar.name} is_vintage() - Expected True - got {guitar.is_vintage()}')
+print(f'{another_guitar.name} is_vintage() - Expected False - got {another_guitar.is_vintage()}')
