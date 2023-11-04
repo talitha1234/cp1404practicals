f93c16f080a275c11f02fcdbc53f4ff335e80b28
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-11-04 14:10:45 +1000

    Add name field point arithmetic in alternate csv reader file in prac 7

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -40,7 +40,7 @@ def main():
         print(language)
 
 
-main()
+# main()
 
 
 def using_csv():
@@ -64,7 +64,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, point_arithmetic')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
@@ -79,11 +79,11 @@ def using_namedtuple():
 
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, point_arithmetic')
     in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):
         print(language.name, 'was released in', language.year)
         print(repr(language))
 
-# using_csv_namedtuple()
+using_csv_namedtuple()
