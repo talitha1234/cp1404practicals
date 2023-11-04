2f0c0f80c70285b9717f5a7cf478c3995f9ea574
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-11-04 21:30:23 +1000

    Add menu to program management prac 7

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,13 +2,14 @@
 Project
 Estimate: 4 hours
 start: 6:50pm
-end:
-Actual: 45 +
+end: 9
+Actual: 45 + 10 + 120 + 20
 """
+from datetime import datetime
 
 
 class Project:
-    def __init__(self, name, start_date, priority, cost_estimate=0.0, completion_percent=0):
+    def __init__(self, name, start_date=datetime, priority=0, cost_estimate=0.0, completion_percent=0):
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -16,15 +17,17 @@ class Project:
         self.completion_percent = completion_percent
 
     def __repr__(self):
-        return str(f'{self.name}, start: {self.start_date}, priority {self.priority}, '
-                   f'estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percent}%')
+        return (f'{self.name}, start: {self.start_date}, priority {self.priority},'
+                f' estimate: ${self.cost_estimate:,.2f}, completion: {self.completion_percent}% ')
+
+
 
     def __lt__(self, other):
-        return self.start_date < other.start_date
+        return (datetime.strptime(self.start_date, "%d/%m/%Y").date() >
+                datetime.strptime(other, "%d/%m/%Y").date())
 
     def is_complete(self):
         return self.completion_percent == 100
-
 
 
 
@@ -32,7 +35,6 @@ def project_tests():
     first_project = Project('Build Car Park', '12/09/2021', 2, 600000.0, 95)
     print(first_project)
     second_project = Project('Mow Lawn', '31/10/2022', 3, 3.0, 0)
-    #TODO figure out how to make the start_date a date and not a string so that this comparison will work
-    # print(first_project < second_project)
+    print(first_project < '31/10/2022')
 
-# project_tests()
+project_tests()
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -15,57 +15,121 @@ DATE_INDEX = 1
 PRIORITY_INDEX = 2
 COST_INDEX = 3
 PERCENT_INDEX = 4
+MENU = ("- (L)oad projects \n"
+        "- (S)ave projects\n"
+        "- (D)isplay projects\n"
+        "- (F)ilter projects by date\n"
+        "- (A)dd new project\n"
+        "- (U)pdate project\n"
+        "- (Q)uit\n"
+        ">>>")
+FILENAME = 'projects.txt'
 
-# load_file = input("Filename as name.txt:")
-projects = []
-with open("projects.txt", 'r') as in_file:
-    # file format: Name	Start Date	Priority	Cost Estimate	Completion Percentage
-    # don't want the first line so just read it
-    in_file.readline()
-    for line in in_file:
+
+def main():
+    projects = process_incoming_records(FILENAME)
+    choice = input(MENU).lower()
+    while choice != 'q':
+        if choice == 'l':
+            load_file = input("Filename as name.txt:")
+            process_incoming_records()
+        elif choice == 's':
+            save_file = input("Filename as name.txt:")
+            process_outgoing_records(projects, save_file)
+        elif choice == 'd':
+            display_projects(projects)
+        elif choice == 'f':
+            # filter_with_date(projects)
+                pass
+        elif choice == 'a':
+            # add_project(projects)
+            pass
+        elif choice == 'u':
+            pass
+        choice = input(MENU).lower()
+    process_outgoing_records(projects, FILENAME)
+print("Thank you for using custom-built project management software.")
+
+
+def add_project(projects):
+    print("Let's add a new project")
+    name = input("Name: ")
+    start_date = input("Start date (dd/mm/yy): ")
+    priority = input("Priority: ")
+    cost = input("Cost estimate: $")
+    percent = input("Percent complete: ")
+    project = Project(name, start_date, priority, cost, percent)
+    projects.append(project)
+
+
+def filter_with_date(projects):
+    # filter by dates
+    # date_string = input("Show projects that start after date (dd/mm/yy): ")  # e.g., "30/9/2022"
+    # Test with:
+    date_string = '30/9/2022'
+    print("date")
+    for project in projects:
+        # compare
+        # object_date = datetime.datetime.strptime(project.start_date, "%d/%m/%Y").date()
+        # date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+        if datetime.strptime(project.start_date, "%d/%m/%Y").date() > datetime.strptime(date_string, "%d/%m/%Y").date():
+            print(project)
+
+
+def display_projects(projects):
+    # display projects from whatever file was most recently loaded and processed
+    print("Incomplete Projects")
+    for project in projects:
+        if not project.is_complete():
+            print(project)
+    print("Completed Projects")
+    for project in projects:
+        if project.is_complete():
+            print(project)
+
+
+
+def process_incoming_records(filename=FILENAME):
+    # load_file = input("Filename as name.txt:")
+    projects = []
+    with open(filename, 'r') as in_file:
+        # file format: Name	Start Date	Priority	Cost Estimate	Completion Percentage
+        # don't want the first line so just read it
+        in_file.readline()
+        lines = in_file.readlines()
+    for line in lines:
         project_parts = line.strip().split('\t')
         # want priority as int and cost as float and percent as int
         project_parts[PRIORITY_INDEX] = int(project_parts[PRIORITY_INDEX])
         project_parts[COST_INDEX] = float(project_parts[COST_INDEX])
         project_parts[PERCENT_INDEX] = int(project_parts[PERCENT_INDEX])
         # want date as date for project class to work
-        project_parts[DATE_INDEX] = datetime.datetime.strptime(project_parts[DATE_INDEX], "%d/%m/%Y").date()
+        # project_parts[DATE_INDEX] = datetime.datetime.strptime(project_parts[DATE_INDEX], "%d/%m/%Y").date()
         project = Project(project_parts[NAME_INDEX], project_parts[DATE_INDEX], project_parts[PRIORITY_INDEX],
                           project_parts[COST_INDEX], project_parts[PERCENT_INDEX])
         projects.append(project)
+    return projects
 
-print(projects)
-
-# display projects
-complete_projects = (project for project in projects if project.is_complete())
-incomplete_projects = (project for project in projects if not project.is_complete())
-
-print("Incomplete Projects")
-for project in incomplete_projects:
-    print(project)
-
-print("Completed Projects")
-for project in complete_projects:
-    print(project)
+def process_outgoing_records(projects, filename):
+    with open(filename, 'w') as out_file:
+        out_file.write('Name	Start Date	Priority	Cost Estimate	Completion Percentage')
+        for project in projects:
+            # date was changed at some point to y-m-d so reformatting
+            # reformatted_date = datetime.datetime.strptime(project[DATE_INDEX], "%d/%m/%Y").date()
+            project_string = (f'{project.name}	{project.start_date}	{project.priority}	{project.cost_estimate}'
+                              f'	{project.completion_percent}\n')
+            out_file.write(project_string)
 
 
-# add new project
-# print("Let's add a new project")
-# name = input("Name: ")
-# start_date = input("Start date (dd/mm/yy): ")
-# priority = input("Priority: ")
-# cost = input("Cost estimate: $")
-# percent = input("Percent complete: ")
-# project = Project(name, start_date, priority, cost, percent)
-# projects.append(project)
-
-# filter by dates
-# date_string = input("Show projects that start after date (dd/mm/yy): ")  # e.g., "30/9/2022"
-date_string = '30/9/2022'
-date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-print("date")
-for project in projects:
-    if project.start_date > date:
-        print(project)
 
 
+
+
+
+
+
+
+
+
+
+main()
