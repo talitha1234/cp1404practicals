8e3406b5246ea0079ffc8fd7d21b9223a6f1639e
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-11-04 17:55:13 +1000

    Process incoming file project management in prac 7

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -8,7 +8,33 @@ Actual:
 
 import datetime
 
-date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
-date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-print(f"That day is/was {date.strftime('%A')}")
-print(date.strftime("%d/%m/%Y"))
+from prac_07.project import Project
+
+NAME_INDEX = 0
+DATE_INDEX = 1
+PRIORITY_INDEX = 2
+COST_INDEX = 3
+PERCENT_INDEX = 4
+
+# date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
+# date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+# print(f"That day is/was {date.strftime('%A')}")
+# print(date.strftime("%d/%m/%Y"))
+
+projects = []
+with open("projects.txt", 'r') as in_file:
+    # file format: Name	Start Date	Priority	Cost Estimate	Completion Percentage
+    # don't want the first line so just read it
+    in_file.readline()
+    for line in in_file:
+        project_parts = line.strip().split('\t')
+        # want priority as int and cost as float and percent as int
+        project_parts[PRIORITY_INDEX] = int(project_parts[PRIORITY_INDEX])
+        project_parts[COST_INDEX] = float(project_parts[COST_INDEX])
+        project_parts[PERCENT_INDEX] = int(project_parts[PERCENT_INDEX])
+
+        project = Project(project_parts[NAME_INDEX], project_parts[DATE_INDEX], project_parts[PRIORITY_INDEX],
+                          project_parts[COST_INDEX], project_parts[PERCENT_INDEX])
+        projects.append(project)
+
+print(projects)
