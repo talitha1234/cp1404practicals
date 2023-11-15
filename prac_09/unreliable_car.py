2f25c242b7364d376e7675adab328f17e1952595
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-11-15 15:15:51 +1000

    Add unreliable car class prac 9

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,29 @@
+"""
+Unreliable Car - Uses Car Class
+Estimate: 15 mins
+Actual: 15 mins
+"""
+import random
+
+from prac_09.car import Car
+
+
+class UnreliableCar(Car):
+    """Unreliable car class"""
+    def __init__(self, name, fuel, reliability: int):
+        """Construct unreliable car"""
+        super().__init__(reliability)
+        self.name = name
+        self.fuel = fuel
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive car if working"""
+        if random.randint(0, 100) < self.reliability:
+            if distance > self.fuel:
+                distance = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+            self.odometer += distance
+        return distance
