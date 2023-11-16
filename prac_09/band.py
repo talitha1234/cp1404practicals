17911ee621d79c94dcdfc0eb2eb20c54f6a8b2e3
talitha1234 <109891749+talitha1234@users.noreply.github.com>
2023-11-16 14:07:10 +1000

    Complete band class to pass tests in prac 9

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,3 +1,36 @@
+"""
+Band for musicians
+Estimate: 60 mins
+Actual: 65 mins
+"""
+import keyword
+
+from tomlkit import string
+
+
 class Band():
-    def __init__(self):
+    """Band Class"""
+
+    def __init__(self, band_name: str):
+        """Construct Band class"""
         self.band_members = []
+        self.band_name = band_name
+
+    def __repr__(self):
+        """Return a string representation of a Musician, showing the variables."""
+        members_string = ','.join(str(member) for member in self.band_members)
+        return f'{self.band_name}({members_string})'
+
+    def add(self, musician):
+        """Add musician to band"""
+        self.band_members.append(musician)
+
+    def play(self):
+        """Return a string showing the instrument playing their first (or no) instrument."""
+        players_playing = []
+        for musician in self.band_members:
+            try:
+                players_playing.append(f"{musician.name} is playing: {musician.instruments[0]}")
+            except IndexError:
+                players_playing.append(f"{musician.name} needs an instrument!")
+        return "\n".join(players_playing)
